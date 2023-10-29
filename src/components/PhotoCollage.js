0f7cbec31871a952e7413c36fbd018bad284b6a3
Sasa Milic <milichkam@gmail.com>
2023-10-29 16:58:48 +0100

    added more pictures

--- src/components/PhotoCollage.js
+++ src/components/PhotoCollage.js
@@ -4,42 +4,74 @@ import { ReactComponent as Title} from '../assets/hawkchild_diy.svg';
 import posterEC from '../assets/collage/2023_EvianChrist/poster.jpg';
 import gifEC from '../assets/collage/2023_EvianChrist/josh.gif';
 import posterSky from '../assets/collage/2023_SkyH1-Woesum/poster.jpeg';
+import gifSkyDj from '../assets/collage/2023_SkyH1-Woesum/dj.gif';
+import gifSkyCrowd from '../assets/collage/2023_SkyH1-Woesum/crowd.gif';
+import posterMarkyB from '../assets/collage/2023_MarkyB-Seretide/poster.jpeg';
+import sooaxka from '../assets/collage/2023_MarkyB-Seretide/sooaxka.png';
 
 function Collage() {
     const mediaList = [
         {
-            type: 'image',
-            url: posterEC,
+	    column: 'left',
+	    url: posterEC,
             customStyles: {
             }
         },
         {
-            type: 'gif',
+	    column: 'right',
             url: gifEC,
             customStyles: {
-                //width: '85%',
-                //margin: '20px auto'
-		marginLeft: '50px',
-                // ... any other custom styles
+		marginLeft: 'calc(1% * 25)',
             }
         },
         {
-            type: 'image',
+	    column: 'left',
+            url: gifSkyDj,
+            customStyles: {
+		paddingTop: 'calc(1% * 30)',
+		marginLeft: 'calc(1% * 25)',
+            }
+        },
+        {
+	    column: 'right',
             url: posterSky,
             customStyles: {
-                width: '70%',
-                margin: '20px auto'
-                // ... any other custom styles
+		paddingTop: 'calc(1% * 10)',
+            }
+        },
+        {
+	    column: 'left',
+            url: gifSkyCrowd,
+            customStyles: {
+		marginTop: 'calc(1% * -30)',
+		marginRight: 'calc(1% * 50)',
+            }
+        },
+        {
+	    column: 'right',
+            url: posterMarkyB,
+            customStyles: {
+		position: 'absolute',
+		paddingTop: 'calc(260%)',
+		marginLeft: 'calc(-50%)',
+            }
+        },
+        {
+	    column: 'right',
+            url: sooaxka,
+            customStyles: {
+		width: '50%',
+		paddingTop: 'calc(50%)',
+		marginLeft: 'calc(50%)',
             }
         }
-        // ... add more as needed
     ];
 
     return (
         <div className="collage-container">
             <div className="column">
-                {mediaList.filter((_, index) => index % 2 === 0).map((media, index) => (
-                    <img 
+                {mediaList.filter(media => media.column === 'left').map((media, index) => (
+                    <img
                         key={index}
                         src={media.url}
                         alt="collage media"
@@ -48,8 +80,8 @@ function Collage() {
                 ))}
             </div>
             <div className="column">
-                {mediaList.filter((_, index) => index % 2 !== 0).map((media, index) => (
-                    <img 
+                {mediaList.filter(media => media.column === 'right').map((media, index) => (
+                    <img
                         key={index}
                         src={media.url}
                         alt="collage media"
