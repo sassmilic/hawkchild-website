d3fcdd994b6a34d92ba437230b0ab5d6c0ec8a8e
Sasa Milic <milichkam@gmail.com>
2023-11-01 13:11:52 +0100

    title svg positioning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 import EmptyScreen from './components/EmptyScreen';
 import { ReactComponent as TitleSVG } from './assets/hawkchild_diy.svg'
@@ -7,19 +7,22 @@ import PixelateFilter from './components/PixelateFilter';
 import InvertFilter from './components/InvertFilter';
 
 function App() {
-    
-    const [maskPosition, setMaskPosition] = useState({ x: 'center', y: 'center' });
-    
+   
+    const svgRef = useRef(null); // for measuring dimensions of title
+    const [maskPosition, setMaskPosition] = useState({ x: 0, y: 0 });
+  
+    const titleAspectRatio = 90.41 / 1094.483; // hardcoded from SVG file
+
     useEffect(() => {
         const adjustMaskPosition = () => {
             const viewportWidth = window.innerWidth;
             const viewportHeight = window.innerHeight;
             const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-            const centerX = `${viewportWidth / 2}px`;
-            const centerY = `${scrollTop + (viewportHeight / 2)}px`;
-
-            setMaskPosition({ x: centerX, y: centerY });
+            const maskHeight = titleAspectRatio * viewportWidth;
+            const centerY = `${scrollTop + (viewportHeight / 2) - (maskHeight / 2)}px`;
+            console.log(maskHeight);
+            console.log(centerY);
+            setMaskPosition({ x: 0, y: centerY});
         };
 
         window.addEventListener('scroll', adjustMaskPosition);
@@ -35,18 +38,21 @@ function App() {
 
     return (
 	<div className="main">
+        // `TitleSVG` is rendered off-screen in order to get dimensions.
+        <TitleSVG style={{ position: 'absolute', top: '-9999px' }} ref={svgRef} />
 	    <PixelateFilter />
 	    <InvertFilter />
-	    { /* <TitleSVG className="title" /> */}
+        /*
 	    <div className="background">
-		<EmptyScreen percentage={50} />
-		<PhotoCollage />
-		<EmptyScreen percentage={100} />
+            <EmptyScreen percentage={50} />
+            <PhotoCollage />
+            <EmptyScreen percentage={100} />
 	    </div>
+        */
 	    <div className="background2" style={{ maskPosition: `${maskPosition.x} ${maskPosition.y}`, WebkitMaskPosition: `${maskPosition.x} ${maskPosition.y}` }}>
-		<EmptyScreen percentage={50} />
-		<PhotoCollage />
-		<EmptyScreen percentage={100} />
+            <EmptyScreen percentage={50} />
+            <PhotoCollage />
+            <EmptyScreen percentage={100} />
 	    </div>
 	</div>
     );
