7f0e2337ae1d99f18e76911dd7346dadd9aea1cc
Sasa Milic <milichkam@gmail.com>
2023-11-06 16:04:03 +0100

    more title debugging

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import HawkchildText from './assets/hawkchild_diy.svg';
 function App() {
   
     return (
-        <>
         <div className="main">
             <div className="title-svg">
                 <img src={HawkchildText} alt="HAWKCHILD   DIY"/>
             </div>
             <PhotoCollage className="background"/>
         </div>
-        </>
     );
 }
 
