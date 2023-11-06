8a6d010ad0da1330dd8a0a9264ee2ab2a55f3a88
Sasa Milic <milichkam@gmail.com>
2023-11-06 15:35:10 +0100

    title bug fixes

--- src/App.js
+++ src/App.js
@@ -1,68 +1,19 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './reset.css';
 import './App.css';
-import EmptyScreen from './components/EmptyScreen';
 import PhotoCollage from './components/PhotoCollage';
-import InvertAndPixelizeFilter from './components/InvertAndPixelizeFilter';
+import HawkchildText from './assets/hawkchild_diy.svg';
 
 function App() {
   
-    const titleAspectRatio = 88 / 679.665; // hardcoded from SVG file
-    const [maskPosition, setMaskPosition] = useState({ x: 0, y: 0 });
-
-    // Define a debounce function
-    function debounce(func, wait) {
-      let timeout;
-      return function executedFunction(...args) {
-        const later = () => {
-          clearTimeout(timeout);
-          func(...args);
-        };
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-      };
-    }
-
-    useEffect(() => {
-        const adjustMaskPosition = () => {
-            const viewportWidth = window.innerWidth;
-            const viewportHeight = window.innerHeight;
-            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-            const maskHeight = titleAspectRatio * viewportWidth;
-            const centerY = `${scrollTop + (viewportHeight / 2) - (maskHeight / 2)}px`;
-            setMaskPosition({ x: 0, y: centerY});
-        };
-
-        // Debounce the adjustMaskPosition function
-        const debouncedAdjustMaskPosition = debounce(adjustMaskPosition, 50);
-
-        adjustMaskPosition(); 
-
-        // Add event listeners using the debounced function
-        window.addEventListener('scroll', debouncedAdjustMaskPosition);
-        window.addEventListener('resize', debouncedAdjustMaskPosition);
-
-        return () => {
-            // Remove the event listeners using the debounced function
-            window.removeEventListener('scroll', debouncedAdjustMaskPosition);
-            window.removeEventListener('resize', debouncedAdjustMaskPosition);
-        };
-    }, []);
-
     return (
         <>
-        <div className="title-svg">
-            <img src="/hawkchild_diy.svg" />
+        <div className="main">
+            <div className="title-svg">
+                <img src={HawkchildText} alt="HAWKCHILD   DIY"/>
+            </div>
+            <PhotoCollage className="background"/>
         </div>
-        <PhotoCollage id="foreground"/>
-        <InvertAndPixelizeFilter />
-        <PhotoCollage
-            id="background"
-            style={{
-                maskPosition: `${maskPosition.x} ${maskPosition.y}`,
-                WebkitMaskPosition: `${maskPosition.x} ${maskPosition.y}`
-            }}
-            />
         </>
     );
 }
