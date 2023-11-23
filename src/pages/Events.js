b7cfd8b56d905930732ab18d363ed6be81ce4c8b
Sasa Milic <milichkam@gmail.com>
2023-11-23 17:52:18 +0100

    default sort by date

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -5,32 +5,39 @@ import './Events.css';
 let data = require('./../data/eventData.json');
 
 const Events = () => {
-  const [tableData, setTableData] = useState(data);
-  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
+    const [sortConfig, setSortConfig] = useState({ key: 'date', direction: 'descending' });
 
-  const onSort = (key) => {
-    let direction = 'ascending';
-    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
-      direction = 'descending';
-    }
-    setSortConfig({ key, direction });
-
-    const sortedData = [...tableData].sort((a, b) => {
-      if (direction === 'ascending') {
-        return a[key].localeCompare(b[key]);
-      } else {
-        return b[key].localeCompare(a[key]);
-      }
+    const sortedData = data.sort((a, b) => {
+        if (sortConfig.direction === 'ascending') {
+            return a[sortConfig.key].localeCompare(b[sortConfig.key]);
+        } else {
+            return b[sortConfig.key].localeCompare(a[sortConfig.key]);
+        }
     });
+    const [tableData, setTableData] = useState(sortedData);
 
-    setTableData(sortedData);
+    const onSort = (key) => {
+        let direction = 'ascending';
+        if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+            direction = 'descending';
+        }
+        setSortConfig({ key, direction });
+
+        const sortedData = [...tableData].sort((a, b) => {
+            if (direction === 'ascending') {
+                return a[key].localeCompare(b[key]);
+            } else {
+                return b[key].localeCompare(a[key]);
+            }
+        });
+        setTableData(sortedData);
   };
 
-  return (
-    <div className="spreadsheet-container">
-      <Table data={tableData} onSort={onSort} sortConfig={sortConfig} />
-    </div>
-  );
+    return (
+        <div className="spreadsheet-container">
+            <Table data={tableData} onSort={onSort} sortConfig={sortConfig} />
+        </div>
+    );
 };
 
 export default Events;
