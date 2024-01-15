f15044b2fa75c367df2501ea597e4f1a8e6190c6
Sasa Milic <milichkam@gmail.com>
2024-01-14 20:08:13 -0500

    minor style changes to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,17 +9,17 @@ function Footer() {
         <img src={Logo} alt="HAWKCHILD___DIY"/>
         <div className="nav-container">
             <a href="/about">About</a>
-            <a href="/">Events</a>
-            <a href="/">Discord</a>
+            <a href="/events">Events</a>
+            <a href="/community">Discord</a>
         </div>
         <div className="socials-container">
             <a href="https://twitter.com/hawkchild" target="_blank" rel="noopener noreferrer">Twitter/X↗</a>
-            <a href="/">Instagram↗</a>
+            <a href="https://www.instagram.com/hawkchild.diy" target="_blank" rel="noopener noreferrer">Instagram↗</a>
         </div>
       </div>
       <div className="footer-right">
         <a href="/">Back to top ↑</a>
-        <p>Designed & built by Saša Milić ©2024</p>
+        <p>Designed & built by <a href="https://twitter.com/realSasaMilic">Saša Milić</a> ©2024</p>
       </div>
     </footer>
   );
