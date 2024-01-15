c836cce815b2562979bbc2244014d2a2f27f395a
Sasa Milic <milichkam@gmail.com>
2024-01-14 19:05:07 -0500

    big footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,22 @@ import Logo from './../assets/logo.jpeg';
 function Footer() {
   return (
     <footer>
-      <img src={Logo} alt="HAWKCHILD___DIY"/>
+      <div className="footer-left">
+        <img src={Logo} alt="HAWKCHILD___DIY"/>
+        <div className="nav-container">
+            <a href="/about">About</a>
+            <a href="/">Events</a>
+            <a href="/">Discord</a>
+        </div>
+        <div className="socials-container">
+            <a href="https://twitter.com/hawkchild" onClick={() => console.log('Twitter link clicked')}>Twitter↗</a>
+            <a href="/">Instagram↗</a>
+        </div>
+      </div>
+      <div className="footer-right">
+        <a href="/">Back to top ↑</a>
+        <p>©2024</p>
+      </div>
     </footer>
   );
 }
---
+++ src/components/ScrollingBanner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ScrollingBanner.css';
+
+const ScrollingBanner = () => {
+    return (
+        <div className="banner-container">
+        {/*
+            <div className="scrolling-banner">
+                Glasgow Coordinates: Latitude 55.8642, Longitude -4.2518
+            </div>
+        */}
+        </div>
+    );
+};
+
+export default ScrollingBanner;
+
