9be901826dcc426688616768737c148539405607
Sasa Milic <milichkam@gmail.com>
2024-01-27 14:09:38 +0100

    contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import './Contact.css';
 
-const AboutPage = () => {
+const ContactPage = () => {
     return (
-        <div>
-            <h1>About Us</h1>
-            <p>This is the about page.</p>
-        </div>
+        <div className="contact-page-container">
+                <h1>Contact</h1>
+                <div className="contact-page-text-wrap">
+                    <div>
+                    <h2>email:</h2><h2>hashim@hawkchild.com</h2>
+                    </div>
+                    <div>
+                    <h2>instagram:</h2><h2>@hawkchild.diy</h2>
+                    </div>
+                    <div>
+                    <h2>twitter:</h2><h2>@hawkchild</h2>
+                    </div>
+                </div>
+            </div>
     );
 };
 
-export default AboutPage;
+export default ContactPage;
 
