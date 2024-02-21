9ba544f467e3274eaca0e39f52ac50bcbe360ad2
Sasa Milic <milichkam@gmail.com>
2024-02-21 15:46:44 +0100

    lazy loading images/videos

--- src/components/ContentLayer.js
+++ src/components/ContentLayer.js
@@ -1,28 +1,58 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './ContentLayer.css';
 
-// Add `idMap` prop for mapping filenames to IDs
 const ContentLayer = ({ images, speed, className, opposite, idMap }) => {
-  // Determine the sort order based on the `opposite` prop
+  const [elementsRef, setElementsRef] = useState({});
+  
+  const observer = useRef(new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const lazyImage = entry.target;
+        if (lazyImage.dataset.src) {
+          lazyImage.src = lazyImage.dataset.src;
+          if (lazyImage.tagName.toLowerCase() === 'video') {
+            lazyImage.load(); // For video, load the video after setting the source
+          }
+          observer.current.unobserve(lazyImage);
+        }
+      }
+    });
+  }, { rootMargin: '0px', threshold: 0.1 }));
+
+  useEffect(() => {
+    const currentObserver = observer.current;
+    Object.values(elementsRef).forEach(el => currentObserver.observe(el));
+    
+    return () => {
+      if (currentObserver) {
+        currentObserver.disconnect();
+      }
+    };
+  }, [elementsRef]);
+
   const sortOrder = (a, b) => {
     const comparison = a[0].localeCompare(b[0], undefined, {numeric: true, sensitivity: 'base'});
-    return opposite ? -comparison : comparison; // Reverse the order if `opposite` is true
+    return opposite ? -comparison : comparison;
   };
 
-  // Sort entries alphanumerically and reverse if `opposite` is true
   const sortedImages = Object.entries(images).sort(sortOrder);
+
+  const setRef = (el) => {
+    if (el && !elementsRef[el.id]) {
+      setElementsRef(prevRefs => ({ ...prevRefs, [el.id]: el }));
+    }
+  };
 
   return (
     <div className={`content-layer ${className}`}>
       {sortedImages.map(([key, value], index) => {
         const isVideo = key.endsWith('.mp4');
-        // Determine if the current file has a specific ID assigned
-        const specificId = idMap && idMap[key] ? idMap[key] : '';
-
+        const specificId = idMap && idMap[key] ? idMap[key] : `element-${index}`;
+        
         return isVideo ? (
-          <video key={index} id={specificId} src={value} alt={`content ${index}`} loop autoPlay muted />
+          <video key={index} id={specificId} data-src={value} alt={`content ${index}`} loop autoPlay muted ref={setRef} />
         ) : (
-          <img key={index} id={specificId} src={value} alt={`content ${index}`} />
+          <img key={index} id={specificId} data-src={value} alt={`content ${index}`} ref={setRef} />
         );
       })}
     </div>
