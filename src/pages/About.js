670895da054f338374a2b0d72d6dc5d9293930eb
Sasa Milic <milichkam@gmail.com>
2024-04-27 17:55:53 +0200

    about page redesign

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ const AboutPage = () => {
     return (
         <div className="about-page-container">
             <div className="about-page-text-wrap">
-                <h1>About Hashim</h1>
+                <h1>About</h1>
                 <p>
     Hawkchild DIY was formed by Hashim Ali at the young age of 15 in his bedroom in the Pollokshields area of Glasgow. Driven by a passion for unorthodox, innovative and interesting music and art forms, the project started off as an events promoting project. As progression continued and the formation of relationships with artists across the globe had snowballed, Hawkchild DIY developed and diversified to go on to release records, producing full scale professional concerts, club nights and European tours along with radio appearances and DJ sets. In 2018, we see the hobby and passion of a school boy mature into a driving force for independent music and art on a larger scale.
                 </p>
