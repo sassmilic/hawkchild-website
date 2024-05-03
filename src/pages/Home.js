e6e3af10c84546b930dd4818f7491dcb578d552d
Sasa Milic <milichkam@gmail.com>
2024-05-03 22:36:00 +0200

    preload title text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import LogoMarquee from "../components/LogoMarquee";
 import ContentMarquee from "../components/ContentMarquee";
 import SimpleFooter from "../components/SimpleFooter";
@@ -19,29 +20,6 @@ function Home() {
   */
 
   const MARQUEE_SPEED = "180s";
-
-  /*
-  const posters = [
-      '/media/hi-res/posters/poster_evian-christ_2024.jpeg',
-      '/media/hi-res/posters/poster_markyb-seretide_2024.jpeg',
-      '/media/hi-res/posters/poster_skyh1-woesum_2024.jpeg',
-      '/media/hi-res/posters/poster_affxworks-clouds_2023.jpeg',
-      '/media/hi-res/posters/poster_dark0-femi_2022.jpeg',
-      '/media/hi-res/posters/poster_doss-olixl_2022.jpeg',
-      '/media/hi-res/posters/poster_casualgabberz-cloudz_2022.jpeg',
-      '/media/hi-res/posters/poster_malibu-mechatok_2022.jpeg',
-      '/media/hi-res/posters/poster_fundraiser_2021.jpeg',
-      '/media/hi-res/posters/poster_tp10_2019.jpeg',
-      '/media/hi-res/posters/poster_nassim_2018.jpeg',
-      '/media/hi-res/posters/poster_tp6_2017.jpeg',
-      '/media/hi-res/posters/poster_dark0_2017.jpeg',
-      '/media/hi-res/posters/poster_yunglean_2016.jpeg',
-      '/media/hi-res/posters/poster_dg_2016.png',
-      '/media/hi-res/posters/poster_kamixlo-mssingno_2016.jpeg',
-      '/media/hi-res/posters/poster_yunglean_2014.jpeg',
-      '/media/hi-res/posters/poster_yunglean_2014.jpeg',
-  ];
-  */
 
   const posters = [
     "/media/poster_evian-christ_2024.jpeg",
@@ -64,29 +42,6 @@ function Home() {
     "/media/poster_yunglean_2014.jpeg",
   ];
 
-  /*
-  const pics = [
-      '/media/edited-images/evian.gif',
-      '/media/edited-images/sooakxa.png',
-      '/media/edited-images/kamixlo-woesum.jpg',
-      '/media/edited-images/strobes.jpg',
-      '/media/edited-images/dark0-dog.jpg',
-      '/media/edited-images/doss-crowd.jpg',
-      '/media/edited-images/cg-leigh.jpg',
-      '/media/edited-images/malibu-mechatok.jpg',
-      '/media/edited-images/guy.jpg',
-      '/media/edited-images/subcity.jpeg',
-      '/media/edited-images/pizza.jpeg',
-      '/media/edited-images/scarves.jpeg',
-      '/media/edited-images/dark0-etal.jpg',
-      '/media/edited-images/yunglean-news.jpg',
-      '/media/edited-images/dg.jpg',
-      '/media/edited-images/kamixlo.jpg',
-      '/media/edited-images/tp-team.png',
-      '/media/edited-images/tp-team.png',
-  ]
-  */
-
   const pics = [
     "/media/evian.gif",
     "/media/sooakxa.png",
@@ -107,57 +62,7 @@ function Home() {
     "/media/tp-team.png",
     "/media/tp-team.png",
   ];
-  /*
-  const gifs = [
-      '/media/gifs/1.gif',
-      '/media/gifs/4.gif',
-      '/media/gifs/2.gif',
-      '/media/gifs/3.gif',
-      '/media/gifs/5.gif',
-      '/media/gifs/6.gif',
-      '/media/gifs/15.gif',
-      '/media/gifs/12.gif', //**
-      '/media/gifs/7.gif',
-      '/media/gifs/8.gif',
-      '/media/gifs/9.gif',
-      '/media/gifs/10.gif',
-      '/media/gifs/11.gif',
-      '/media/gifs/12.gif', //**
-      '/media/gifs/13.gif', //**
-      '/media/gifs/14.gif',
-      '/media/gifs/16.gif',
-      '/media/gifs/17.gif',
-      '/media/gifs/18.gif',
-      '/media/gifs/19.gif',
-      '/media/gifs/20.gif',
-      '/media/gifs/21.gif',
-      '/media/gifs/22.gif',
-      '/media/gifs/1.gif',
-      '/media/gifs/4.gif',
-      '/media/gifs/2.gif',
-      '/media/gifs/3.gif',
-      '/media/gifs/5.gif',
-      '/media/gifs/6.gif',
-      '/media/gifs/15.gif',
-      '/media/gifs/12.gif', //**
-      '/media/gifs/7.gif',
-      '/media/gifs/8.gif',
-      '/media/gifs/9.gif',
-      '/media/gifs/10.gif',
-      '/media/gifs/11.gif',
-      '/media/gifs/12.gif', //**
-      '/media/gifs/13.gif', //**
-      '/media/gifs/14.gif',
-      '/media/gifs/16.gif',
-      '/media/gifs/17.gif',
-      '/media/gifs/18.gif',
-      '/media/gifs/19.gif',
-      '/media/gifs/20.gif',
-      '/media/gifs/21.gif',
-      '/media/gifs/22.gif',
 
-  ]
-  */
   const gifs = [
     "/media/4.gif",
     "/media/1.gif",
@@ -209,6 +114,11 @@ function Home() {
 
   return (
     <>
+      <Helmet>
+        <link rel="preload" href={DiyText} as="image" type="image/svg+xml" />
+        <link rel="preload" href={HawkText} as="image" type="image/svg+xml" />
+        <link rel="preload" href={ChildText} as="image" type="image/svg+xml" />
+      </Helmet>
       <div className="background-noise"></div>
       <div className="viewport">
         <div className="title-text hawk-svg">
