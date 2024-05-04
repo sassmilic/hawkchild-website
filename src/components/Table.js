f4ee73a51836614097fe70c89dcccc19d1191019
Sasa Milic <milichkam@gmail.com>
2024-05-04 18:47:26 +0200

    responsive event page

--- src/components/Table.js
+++ src/components/Table.js
@@ -13,60 +13,62 @@ const Table = ({ data, onSort, sortConfig }) => {
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th onClick={() => onSort("date")}>
-            <div className="table-header-cell">
-              Date
-              <span
-                className={`sort-indicator ${getSortIndicator("date")}`}
-              ></span>
-            </div>
-          </th>
-          <th onClick={() => onSort("event")}>
-            <div className="table-header-cell">
-              Event
-              <span
-                className={`sort-indicator ${getSortIndicator("event")}`}
-              ></span>
-            </div>
-          </th>
-          <th onClick={() => onSort("artist")}>
-            <div className="table-header-cell">
-              Artist(s)
-              <span
-                className={`sort-indicator ${getSortIndicator("artist")}`}
-              ></span>
-            </div>
-          </th>
-          <th onClick={() => onSort("location")}>
-            <div className="table-header-cell">
-              Location
-              <span
-                className={`sort-indicator ${getSortIndicator("location")}`}
-              ></span>
-            </div>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {Array.isArray(data) &&
-          data.map((item, index) => (
-            <tr
-              key={index}
-              onClick={() => {
-                window.open(item.url, "_blank");
-              }}
-            >
-              <td>{item.date}</td>
-              <td>{item.event}</td>
-              <td>{item.artist}</td>
-              <td>{item.location}</td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th onClick={() => onSort("date")}>
+              <div className="table-header-cell">
+                Date
+                <span
+                  className={`sort-indicator ${getSortIndicator("date")}`}
+                ></span>
+              </div>
+            </th>
+            <th onClick={() => onSort("event")}>
+              <div className="table-header-cell">
+                Event
+                <span
+                  className={`sort-indicator ${getSortIndicator("event")}`}
+                ></span>
+              </div>
+            </th>
+            <th onClick={() => onSort("artist")}>
+              <div className="table-header-cell">
+                Artist(s)
+                <span
+                  className={`sort-indicator ${getSortIndicator("artist")}`}
+                ></span>
+              </div>
+            </th>
+            <th onClick={() => onSort("location")}>
+              <div className="table-header-cell">
+                Location
+                <span
+                  className={`sort-indicator ${getSortIndicator("location")}`}
+                ></span>
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(data) &&
+            data.map((item, index) => (
+              <tr
+                key={index}
+                onClick={() => {
+                  window.open(item.url, "_blank");
+                }}
+              >
+                <td>{item.date}</td>
+                <td>{item.event}</td>
+                <td>{item.artist}</td>
+                <td>{item.location}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
