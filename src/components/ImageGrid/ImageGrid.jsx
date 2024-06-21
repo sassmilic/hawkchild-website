2f9b9aefb17f021698c9e89a6129206cbddc0380
Sasa Milic <milichkam@gmail.com>
2024-06-21 16:28:17 +0200

    more images to image grid

--- src/components/ImageGrid/ImageGrid.jsx
+++ src/components/ImageGrid/ImageGrid.jsx
@@ -3,15 +3,19 @@ import "./ImageGrid.css";
 import EmergingImage from "../EmergingImage/EmergingImage";
 import Scene from "../Scene";
 
-import dog from "../dog.jpeg";
-
 //import reemfields1 from "./imgs/reemfields_1.jpg";
 import reemfields2 from "./imgs/reemfields_2.jpg";
 import reemfields3 from "./imgs/reemfields_3.jpg";
-import evian from "./imgs/evian.mp4";
+import evian from "./imgs/evian-edited.mp4";
 import crowd1 from "./imgs/crowd_1.jpg";
 import logo from "./imgs/logo.png";
 import evian_poster from "./imgs/evian_poster.jpg";
+import markyb_poster from "./imgs/marky_b_seretide_poster.jpg";
+import markyb from "./imgs/markyb.jpg";
+import suitcase from "./imgs/suitcase.jpg";
+import kamixlo_woesum from "./imgs/kamixlo_woesum.jpg";
+import skyh1_woesum_poster from "./imgs/skyh1_woesum_poster.jpg";
+import woesum_dj from "./imgs/woesum_dj-edited.mp4";
 
 function ImageGrid() {
   return (
@@ -28,7 +32,7 @@ function ImageGrid() {
         </figure>
         <figure className="grid__item" style={{ "--r": 2, "--c": 5, "--s": 3 }}>
           <div className="grid__item-img">
-            <EmergingImage url={evian} className="grid__item-img-inner" />
+            <EmergingImage url={reemfields3} className="grid__item-img-inner" />
           </div>
           <figcaption className="grid__item-caption">
             <h3>Lorem ipsum dolor sit amet</h3> <span>2023</span>
@@ -36,7 +40,7 @@ function ImageGrid() {
         </figure>
         <figure className="grid__item" style={{ "--r": 3, "--c": 3, "--s": 3 }}>
           <div className="grid__item-img">
-            <EmergingImage url={reemfields3} className="grid__item-img-inner" />
+            <EmergingImage url={evian} className="grid__item-img-inner" />
           </div>
           <figcaption className="grid__item-caption">
             <h3>Lorem ipsum dolor sit amet</h3> <span>2023</span>
@@ -77,9 +81,9 @@ function ImageGrid() {
             <h3>Lorem ipsum dolor sit amet</h3> <span>2023</span>
           </figcaption>
         </figure>
-        <figure className="grid__item" style={{ "--r": 8, "--c": 6, "--s": 2 }}>
+        <figure className="grid__item" style={{ "--r": 8, "--c": 6, "--s": 4 }}>
           <div className="grid__item-img">
-            <EmergingImage url={dog} className="grid__item-img-inner" />
+            <EmergingImage url={markyb} className="grid__item-img-inner" />
           </div>
           <figcaption className="grid__item-caption">
             <h3>Lorem ipsum dolor sit amet</h3> <span>2023</span>
@@ -87,7 +91,10 @@ function ImageGrid() {
         </figure>
         <figure className="grid__item" style={{ "--r": 9, "--c": 1, "--s": 5 }}>
           <div className="grid__item-img">
-            <EmergingImage url={dog} className="grid__item-img-inner" />
+            <EmergingImage
+              url={markyb_poster}
+              className="grid__item-img-inner"
+            />
           </div>
           <figcaption className="grid__item-caption">
             <h3>Lorem ipsum dolor sit amet</h3> <span>2023</span>
@@ -98,7 +105,7 @@ function ImageGrid() {
           style={{ "--r": 10, "--c": 6, "--s": 3 }}
         >
           <div className="grid__item-img">
-            <EmergingImage url={dog} className="grid__item-img-inner" />
+            <EmergingImage url={suitcase} className="grid__item-img-inner" />
           </div>
           <figcaption className="grid__item-caption">
             <h3>Lorem ipsum dolor sit amet</h3> <span>2023</span>
@@ -109,7 +116,10 @@ function ImageGrid() {
           style={{ "--r": 11, "--c": 4, "--s": 2 }}
         >
           <div className="grid__item-img">
-            <EmergingImage url={dog} className="grid__item-img-inner" />
+            <EmergingImage
+              url={kamixlo_woesum}
+              className="grid__item-img-inner"
+            />
           </div>
           <figcaption className="grid__item-caption">
             <h3>Lorem ipsum dolor sit amet</h3> <span>2023</span>
@@ -120,7 +130,10 @@ function ImageGrid() {
           style={{ "--r": 12, "--c": 1, "--s": 3 }}
         >
           <div className="grid__item-img">
-            <EmergingImage url={dog} className="grid__item-img-inner" />
+            <EmergingImage
+              url={skyh1_woesum_poster}
+              className="grid__item-img-inner"
+            />
           </div>
           <figcaption className="grid__item-caption">
             <h3>Lorem ipsum dolor sit amet</h3> <span>2023</span>
@@ -131,7 +144,7 @@ function ImageGrid() {
           style={{ "--r": 13, "--c": 4, "--s": 5 }}
         >
           <div className="grid__item-img">
-            <EmergingImage url={dog} className="grid__item-img-inner" />
+            <EmergingImage url={woesum_dj} className="grid__item-img-inner" />
           </div>
           <figcaption className="grid__item-caption">
             <h3>Lorem ipsum dolor sit amet</h3> <span>2023</span>
